9d7cb6ec7873e21c72b0ba09c3d9cbfac990f9d6
HANSEL CARZERLET <124737107+hansel112@users.noreply.github.com>
2025-07-03 00:44:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(XylophoneApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildTile({Color color = Colors.white, int soundNumber = 0}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Text(''),
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Xylophone',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.brown,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.grey[100],
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  buildTile(color: Colors.red, soundNumber: 1),
+                  buildTile(color: Colors.orange, soundNumber: 2),
+                  buildTile(color: Colors.yellow, soundNumber: 3),
+                  buildTile(color: Colors.green, soundNumber: 4),
+                  buildTile(color: Colors.teal, soundNumber: 5),
+                  buildTile(color: Colors.blue, soundNumber: 6),
+                  buildTile(color: Colors.purple, soundNumber: 7),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
